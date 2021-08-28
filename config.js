187d50aab54210296932003684e8c9c6956e51b1
unknown <abdulrhman.alshahin@outlook.com>
2021-08-28 05:05:27 +0300

    config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DATABASE:
+    "mongodb+srv://abdulrhman:wpSD8AM68SN90VhF@cluster0.fjduq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
